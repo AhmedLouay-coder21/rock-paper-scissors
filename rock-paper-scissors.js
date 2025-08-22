91dd5d45c5b8d7e8fafbb5b6f4d5640c0fc1ac1f
AhmedLouay-coder21 <140275085+AhmedLouay-coder21@users.noreply.github.com>
2025-08-22 19:53:17 +0300

    fix: correct typo in getComputerChoice function, change first string letters to small

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,7 +1,7 @@
 function getComputerChoice()
 {
     // declare the string domain
-    const string = ["Rock", "Paper", "scissors"];
+    const string = ["rock", "paper", "scissors"];
     // return “rock”, “paper” or “scissors” randomly
     let randomNumber = Math.floor(Math.random() * string.length)
     // print number
