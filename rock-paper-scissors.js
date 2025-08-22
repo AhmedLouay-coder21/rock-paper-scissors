6100534a849fad364623833f94440d0a66ba44e5
AhmedLouay-coder21 <140275085+AhmedLouay-coder21@users.noreply.github.com>
2025-08-22 20:20:34 +0300

    feat: validate user input function implementation

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -9,12 +9,16 @@ function getComputerChoice()
 }
 function getHumanChoice ()
 {
-    // declare the string domain
-    const string = ["rock", "paper", "scissors"];
     //ask user to enter one of “rock”, “paper” or “scissors”
     let userInput = prompt("Enter your desired weapon");
     // for debugging purposes
     console.log(userInput);
+    return userInput;
+}
+function checkUserInput (userInput)
+{
+    // declare the string domain
+    const string = ["rock", "paper", "scissors"];
     // if the user didn't enter anything or entered something unexpected ask him again for a prompt
     while(userInput === null || userInput === '' || !string.includes(userInput.toLowerCase()))
     {
