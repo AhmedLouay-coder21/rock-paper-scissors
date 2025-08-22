3ecb081aeac3e056c06093954d07ff7082c3e52b
AhmedLouay-coder21 <140275085+AhmedLouay-coder21@users.noreply.github.com>
2025-08-22 20:10:57 +0300

    feat: take the user input

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,7 +3,21 @@ function getComputerChoice()
     // declare the string domain
     const string = ["rock", "paper", "scissors"];
     // return “rock”, “paper” or “scissors” randomly
-    let randomNumber = Math.floor(Math.random() * string.length)
+    let randomNumber = Math.floor(Math.random() * string.length);
     // print number
     return string[randomNumber];
+}
+function getHumanChoice ()
+{
+    // declare the string domain
+    const string = ["rock", "paper", "scissors"];
+    //ask user to enter one of “rock”, “paper” or “scissors”
+    let userInput = prompt("Enter your desired weapon");
+    // for debugging purposes
+    console.log(userInput);
+    // if the user didn't enter anything or entered something unexpected ask him again for a prompt
+    while(userInput === null || userInput === '' || !string.includes(userInput.toLowerCase()))
+    {
+        userInput = prompt("Another One!");
+    }
 }
