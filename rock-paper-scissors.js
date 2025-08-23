91ec7b98e4dda1561c681fdda0826c0e4edde2c0
AhmedLouay-coder21 <140275085+AhmedLouay-coder21@users.noreply.github.com>
2025-08-23 17:59:15 +0300

    feat: Implement play round function and improve userInput to deal with camalCase

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,15 +4,16 @@ function getComputerChoice()
     const string = ["rock", "paper", "scissors"];
     // return “rock”, “paper” or “scissors” randomly
     let randomNumber = Math.floor(Math.random() * string.length);
-    // print number
+    // return computer's choice
     return string[randomNumber];
 }
-function getHumanChoice ()
+function getHumanChoice()
 {
     //ask user to enter one of “rock”, “paper” or “scissors”
     let userInput = prompt("Enter your desired weapon");
     // for debugging purposes
     console.log(userInput);
+    userInput = userInput.toLowerCase();
     return userInput;
 }
 function checkUserInput (userInput)
@@ -23,5 +24,50 @@ function checkUserInput (userInput)
     while(userInput === null || userInput === '' || !string.includes(userInput.toLowerCase()))
     {
         userInput = prompt("Another One!");
+    }
+    return userInput;
+}
+let humanScore = 0;
+let computerScore = 0;
+function playRound (getComputerChoice,checkUserInput)
+{
+    // getting computer's choice from getComputerChoice function
+    let computerChoice = getComputerChoice();
+    // getting human's choice from getHumanChoice function
+    let humanChoice = checkUserInput(getHumanChoice());
+    // comparing the two choices
+    if(computerChoice == "rock" && humanChoice == "scissors")
+    {
+        computerScore++;
+        console.log("You lose! rock always beats scissors!")
+    }
+    else if (computerChoice == "rock" && humanChoice == "paper")
+    {
+        humanScore++;
+        console.log("You won! and paper always beats rock")
+    }
+    else if (computerChoice == "scissors" && humanChoice == "rock")
+    {
+        humanScore++;
+        console.log("You won! you are ROCKING!")
+    }
+    else if (computerChoice == "scissors" && humanChoice == "paper")
+    {
+        computerScore++;
+        console.log("auh you lost, you could've won that!")
+    }
+    else if (computerChoice == "paper" && humanChoice == "scissors")
+    {
+        humanScore++;
+        console.log("You win, FATALITY!");
+    }
+    else if (computerChoice == "paper" && humanChoice == "rock")
+    {
+        computerScore++;
+        console.log("You lost,He folded you like a paper!");
+    }
+    else
+    {
+        console.log("It's a tie!, who would've thought that?");
     }
 }
